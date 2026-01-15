cd8a2116244df801c42763bea541e035dc177b92
subrotomojumder <subrotomojumder03@gmail.com>
2026-01-15 22:45:49 +0600

    extra plm practice

---
+++ extra_practice.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+int cmp(Student i, Student j)
+{
+    return i.marks == j.marks ? i.roll > j.roll : i.marks > j.marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    Student arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
+    }
+    sort(arr, arr + n, cmp);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i].name << " " << arr[i].roll << " " << arr[i].marks << endl;
+    }
+    return 0;
+}
